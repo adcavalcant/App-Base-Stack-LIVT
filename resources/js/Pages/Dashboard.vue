b1272107406ceae6c9b6fade51faf2a7d1d2ac3b
adcavalcant <aderc19@gmail.com>
2023-12-16 18:05:29 -0400

    Refatorando o projeto, removendo o vite.js para utilização do laravel mix + configuração do Vue.js

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Olá Mundo!</h1>
+    <blockquote>{{ title }}</blockquote>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+    }
+}
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/inertia-vue3";
+
+const pages = require.context("./Pages/", true, /\.vue$/i);
+const resolveComponent = (name) => {
+    return pages(`./${name}.vue`).default;
+};
+
+createInertiaApp({
+    resolve: (name) => resolveComponent(name),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el);
+    },
+});
